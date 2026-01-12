44c1ea99da5dc8f93994aa68fb2e8a7da7fa07e1
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-01-12 16:13:35 -0500

    lesson finished

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -4,12 +4,18 @@ import TodoList from './TodoList.vue'
 </script>
 
 <template>
-  <div class="todo-app">
-    <h1>To Do List</h1>
-    <todo-form />
-    <todo-list />
-  </div>
+    <div class="todo-app">
+        <h1>To Do List</h1>
+        <todo-form />
+        <todo-list />
+    </div>
 </template>
 
 <style scoped>
+.todo-app {
+    padding: 5vh;
+    min-height: 100vh;
+    width: 30vw;
+    background-color: #f7f9fc;
+}
 </style>

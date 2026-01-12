007783f635ea4fc764745e1280e98b0431a32e1f
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-01-12 16:01:17 -0500

    lesson finished

--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -1,18 +1,24 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useTodoListStore = defineStore('todoList', {
+export const useTodoListStore = defineStore("todoList", {
   state: () => ({
     todoList: [],
-    id: 0
+    id: 0,
   }),
   actions: {
     addTodo(item) {
-      this.todoList.push({ item, id: this.id++, completed: false })
+      this.todoList.push({ item, id: this.id++, completed: false });
     },
     deleteTodo(itemId) {
       this.todoList = this.todoList.filter((object) => {
-        return object.id !== itemId
-      })
-    }
-  }
-})
+        return object.id !== itemId;
+      });
+    },
+    toggleCompleted(idToFind) {
+      const todo = this.todoList.find((obj) => obj.id === idToFind);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+  },
+});
